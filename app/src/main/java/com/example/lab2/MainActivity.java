48d0d76b118d4ce6621b83a7d3978d477b7ef6c7
mdespe <mikespe23@gmail.com>
2020-02-05 13:19:56 -0600

    step 9

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+
+import android.util.Log;
+
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        Log.i("Info", "Button Pressed");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
