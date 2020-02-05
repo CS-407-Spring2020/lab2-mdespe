32e776c03b5b0ef79561af176178dab11e744456
mdespe <mikespe23@gmail.com>
2020-02-05 13:48:06 -0600

    step 13

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -7,11 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
         Log.i("Info", "Button Pressed");
+        EditText myTextField = findViewById(R.id.input_plaintext);
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
     }
 
     @Override
